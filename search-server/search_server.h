a04eb1b646e7cfc97d518bd6ead82a18b09edadb
Nikita <137328449+Z1rea3L@users.noreply.github.com>
2024-02-02 12:20:48 +0200

    Update search_server.h исправления

--- search-server/search_server.h
+++ search-server/search_server.h
@@ -15,7 +15,7 @@ const int MAX_RESULT_DOCUMENT_COUNT = 5;
 
 class SearchServer {
 public:
-
+    
     template <typename StringContainer>
     explicit SearchServer(const StringContainer& stop_words)
     : stop_words_(MakeUniqueNonEmptyStrings(stop_words))
@@ -104,35 +104,5 @@ private:
 
     template <typename DocumentPredicate>
     vector<Document> FindAllDocuments(const Query& query,
-                                                    DocumentPredicate document_predicate) const {
-    map<int, double> document_to_relevance;
-    for (const string& word : query.plus_words) {
-        if (word_to_document_freqs_.count(word) == 0) {
-            continue;
-        }
-        const double inverse_document_freq = ComputeWordInverseDocumentFreq(word);
-        for (const auto& [document_id, term_freq] : word_to_document_freqs_.at(word)) {
-            const auto& document_data = documents_.at(document_id);
-            if (document_predicate(document_id, document_data.status, document_data.rating)) {
-                document_to_relevance[document_id] += term_freq * inverse_document_freq;
-            }
-        }
-    }
-
-    for (const string& word : query.minus_words) {
-        if (word_to_document_freqs_.count(word) == 0) {
-            continue;
-        }
-        for (const auto& [document_id, _] : word_to_document_freqs_.at(word)) {
-            document_to_relevance.erase(document_id);
-        }
-    }
-
-    vector<Document> matched_documents;
-    for (const auto& [document_id, relevance] : document_to_relevance) {
-        matched_documents.push_back(
-            {document_id, relevance, documents_.at(document_id).rating});
-    }
-    return matched_documents;
-}
+                                                    DocumentPredicate document_predicate) const;
 };
